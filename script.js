2fde3fcb85ca9c606de961332cdef9113c562aad
Peteris <blankfeldspeteris@gmail.com>
2022-11-02 20:41:37 +0200

    Function that allow players to add marks to a specific spot on the .field

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
-const Gameboard = (function() {
-    
-    const Player = (symbol) => {
-        const getSymbol = () => symbol;
-        return {getSymbol};
-    }
+const Player = (symbol) => {
+    const getSymbol = () => symbol;
+    return {getSymbol};
+}
 
-    let gameboard = ['x','x','x','x','','','',''];
+const player1 = Player('x');
+const player2 = Player('o');
 
-    const render = (function() {
-        const fields = document.querySelectorAll('.field');
+const Gameboard = (() => {
+    let gameboard = ['','','','','','','',''];
+    let lastSymbol;
+    const fields = document.querySelectorAll('.field');
 
+    const render = () => {
         fields.forEach((field, index) => {
             field.textContent = gameboard[`${index}`];
         });
-    })();
+    };
+
+
+    
+    const addSymbol = () => {
+        fields.forEach((field, index) => {
+            field.addEventListener('click', (e) => {
+                if(e.target.textContent) {
+                   return; 
+                }
+
+                if(lastSymbol === 'x'){
+                    gameboard[`${index}`] = player2.getSymbol();
+                    render();
+                        lastSymbol = player2.getSymbol();
+                } else {
+                    gameboard[`${index}`] = player1.getSymbol();
+                    render();
+                        lastSymbol = player1.getSymbol();
+                }
+            });
+        });
+    }
+
+    fields.forEach(field => {
+        field.addEventListener('click', addSymbol());
+    })
+
 })();
 
-    const gameFlow = (function() {
+const gameFlow = (() => {
+
 })();
