98b8631f81ff5f213e73691f701a2f16c98d198a
Peteris <blankfeldspeteris@gmail.com>
2022-11-08 18:28:25 +0200

    Built function that checks which symbol has won

--- script.js
+++ script.js
@@ -3,12 +3,9 @@ const Player = (symbol) => {
     return {getSymbol};
 }
 
-const player1 = Player('x');
-const player2 = Player('o');
-
 const Gameboard = (() => {
-    let gameboard = ['','','','','','','',''];
     let lastSymbol;
+    let gameboard = ['','','','','','','',''];
     const fields = document.querySelectorAll('.field');
 
     const render = () => {
@@ -17,34 +14,80 @@ const Gameboard = (() => {
         });
     };
 
+    const toggleBoard = () => {
+        fields.forEach((field) => {
+            field.classList.toggle('disable');
+        });
+    }
 
-    
     const addSymbol = () => {
         fields.forEach((field, index) => {
             field.addEventListener('click', (e) => {
+                //If its already taken you can't place
                 if(e.target.textContent) {
                    return; 
                 }
 
                 if(lastSymbol === 'x'){
-                    gameboard[`${index}`] = player2.getSymbol();
+                    gameboard[`${index}`] = gameFlow.player2.getSymbol();
                     render();
-                        lastSymbol = player2.getSymbol();
+                        lastSymbol = gameFlow.player2.getSymbol();
                 } else {
-                    gameboard[`${index}`] = player1.getSymbol();
+                    gameboard[`${index}`] = gameFlow.player1.getSymbol();
                     render();
-                        lastSymbol = player1.getSymbol();
+                        lastSymbol = gameFlow.player1.getSymbol();
                 }
+                //After placement checks who has won
+                gameFlow.checkWin(gameboard, lastSymbol);
             });
         });
+        
     }
 
     fields.forEach(field => {
         field.addEventListener('click', addSymbol());
-    })
+    });
+
+    return {toggleBoard};
 
 })();
 
 const gameFlow = (() => {
+    const player1 = Player('x');
+    const player2 = Player('o');
 
+
+    const checkWin = (board, symbol) => {
+       let result;
+
+        if((board[0] === symbol & board[1] === symbol & board[2] === symbol) ||
+           (board[3] === symbol & board[4] === symbol & board[5] === symbol) ||
+           (board[6] === symbol & board[7] === symbol & board[8] === symbol) ||
+           (board[0] === symbol & board[3] === symbol & board[6] === symbol) ||
+           (board[1] === symbol & board[4] === symbol & board[7] === symbol) ||
+           (board[2] === symbol & board[5] === symbol & board[8] === symbol) ||
+           (board[0] === symbol & board[4] === symbol & board[8] === symbol) ||
+           (board[2] === symbol & board[4] === symbol & board[6] === symbol)
+        ) {
+            result = `${symbol} player won`;
+            Gameboard.toggleBoard();
+            console.log(result);
+            return result;
+        }
+
+        //Checks if its a draw
+        if(board[0] !== '' & board[1] !== '' & board[2] !== '' & board[3] !== '' & 
+           board[4] !== '' & board[5] !== '' & board[6] !== '' & board[7] !== '' & board[8] !== '' ) {
+            result = `It's a draw`;
+            console.log(result);
+            return result;
+        }
+    };
+
+
+    return {
+        player1,
+        player2,
+        checkWin
+    };
 })();
